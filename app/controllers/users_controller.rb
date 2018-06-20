3d6ab1b92c74b5fcded49bab68f13e526ebc9771
Dongseop Kwon <dongseop@gmail.com>
2018-06-21 02:08:45 +0900

    delete unnecessary migration

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -22,7 +22,7 @@ class UsersController < ApplicationController
 
   # GET /users/new
   def new
-    @user = User.new
+    redirect_to @current_user
   end
 
   # GET /users/1/edit
@@ -63,19 +63,19 @@ class UsersController < ApplicationController
 
   # POST /users
   # POST /users.json
-  def create
-    @user = User.new(user_params)
-    return
-    respond_to do |format|
-      if @user.save
-        format.html { redirect_to @user, notice: 'User was successfully created.' }
-        format.json { render :show, status: :created, location: @user }
-      else
-        format.html { render :new }
-        format.json { render json: @user.errors, status: :unprocessable_entity }
-      end
-    end
-  end
+  # def create
+  #   @user = User.new(user_params)
+  #   return
+  #   respond_to do |format|
+  #     if @user.save
+  #       format.html { redirect_to @user, notice: 'User was successfully created.' }
+  #       format.json { render :show, status: :created, location: @user }
+  #     else
+  #       format.html { render :new }
+  #       format.json { render json: @user.errors, status: :unprocessable_entity }
+  #     end
+  #   end
+  # end
 
   # PATCH/PUT /users/1
   # PATCH/PUT /users/1.json
--- db/migrate/20180620155335_delete_spam_users.rb
+++
@@ -1,8 +0,0 @@
-class DeleteSpamUsers < ActiveRecord::Migration[5.0]
-  def change
-    User.all.each do |u|
-      next if u.collections.size > 0
-      u.destroy      
-    end
-  end
-end
